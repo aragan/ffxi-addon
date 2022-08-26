e4d52138fa4d8074d591b308f135535c015e580c
aragan <64033062+aragan@users.noreply.github.com>
2022-08-26 04:29:22 +0300

    Add files via upload

--- Treasury/Treasury.lua
+++ Treasury/Treasury.lua
@@ -1,6 +1,6 @@
 _addon.name = 'Treasury'
 _addon.author = 'Ihina'
-_addon.version = '1.2.0.2'
+_addon.version = '1.2.1.1'
 _addon.commands = {'treasury', 'tr'}
 
 res = require('resources')
@@ -19,22 +19,46 @@ defaults.Verbose = false
 
 settings = config.load(defaults)
 
-ids = T{}
+all_ids = T{}
 for item in res.items:it() do
-    ids[item.name:lower()] = item.id 
-    ids[item.name_log:lower()] = item.id 
+    local name = item.name:lower()
+    if not all_ids[name] then
+        all_ids[name] = S{}
+    end
+    local name_log = item.name_log:lower()
+    if not all_ids[name_log] then
+        all_ids[name_log] = S{}
+    end
+    all_ids[name]:add(item.id)
+    all_ids[name_log]:add(item.id)
 end
 
-s = S{'pass', 'lot', 'drop'}
 code = {}
 code.pass = S{}
 code.lot = S{}
 code.drop = S{}
 
+local flatten = function(s)
+    return s:reduce(function(s1, s2)
+        return s1 + s2
+    end, S{})
+end
+
+local extract_ids = function(settings_table, key)
+    local valid = settings_table[key]:filter(function(name)
+        local found = all_ids[name:lower()] ~= nil
+        if not found then
+            print('Treasury: Item "%s" not found in %s list.':format(name, key))
+        end
+        return found
+    end)
+    return flatten(valid:map(table.get+{all_ids} .. string.lower))
+end
+
 config.register(settings, function(settings_table)
-    code.pass = settings_table.Pass:map(table.get+{ids} .. string.lower)
-    code.lot = settings_table.Lot:map(table.get+{ids} .. string.lower)
-    code.drop = settings_table.Drop:map(table.get+{ids} .. string.lower)
+    code.pass = extract_ids(settings_table, 'Pass')
+    code.lot = extract_ids(settings_table, 'Lot')
+    code.drop = extract_ids(settings_table, 'Drop')
 end)
 
 lotpassdrop_commands = T{
@@ -105,7 +129,7 @@ function force_check()
     -- Check inventory for unwanted items
     if settings.AutoDrop then
         for index, item in pairs(items.inventory) do
-            if type(item) == 'table' and code.drop:contains(item.id) then
+            if type(item) == 'table' and code.drop:contains(item.id) and item.status == 0 then
                 drop(item.id, index, item.count)
             end
         end
@@ -126,13 +150,13 @@ end
 function find_id(name)
     if name == 'pool' then
         return pool_ids()
-        
+
     elseif name == 'seals' then
         return S{1126, 1127, 2955, 2956, 2957}
-        
+
     elseif name == 'currency' then
         return S{1449, 1450, 1451, 1452, 1453, 1454, 1455, 1456, 1457}
-    
+
     elseif name == 'geodes' then
         return S{3297, 3298, 3299, 3300, 3301, 3302, 3303, 3304}
 
@@ -142,8 +166,23 @@ function find_id(name)
     elseif name == 'crystals' then
         return S{4096, 4097, 4098, 4099, 4100, 4101, 4102, 4103}
 
+    elseif name == 'detritus' then
+        return S{9875, 9876}
+
+    elseif name == 'heroism' then
+        return S{9877, 9878}
+
+    elseif name == 'moldy' then
+        return S{9773, 9830, 9831, 9832, 9833, 9834, 9835, 9836, 9837, 9838, 9839, 9840, 9841, 9843, 9868, 9869, 9870, 9871, 9872, 9873, 9874}
+
+    elseif name == 'dynad' then
+        return S{9538, 9539, 9540, 9541, 9542, 9543, 9844, 9845}
+
+    elseif name == 'papers' then
+        return S{9544, 9545, 9546, 9547, 9548, 9549, 9550, 9551, 9552, 9553, 9554, 9555, 9556, 9557, 9558, 9559, 9560, 9561, 9562, 9563, 9564, 9565, 9566, 9567, 9568, 9569, 9570, 9571, 9572, 9573, 9574, 9575, 9576, 9577, 9578, 9579, 9580, 9581, 9582, 9583, 9584, 9585, 9586, 9587, 9588, 9589, 9590, 9591, 9592, 9593, 9594, 9595, 9596, 9597, 9598, 9599, 9600, 9601, 9602, 9603, 9604, 9605, 9606, 9607, 9608, 9609, 9610, 9611, 9612, 9613, 9614, 9615, 9616, 9617, 9618, 9619, 9620, 9621, 9622, 9623, 9624, 9625, 9626, 9627, 9628, 9629, 9630, 9631, 9632, 9633, 9634, 9635, 9636, 9637, 9638, 9639, 9640, 9641, 9642, 9643, 9644, 9645, 9646, 9647, 9648, 9649, 9650, 9651, 9652, 9653, 9654, 9655, 9656, 9657, 9658, 9659, 9660, 9661, 9662, 9663, 9664, 9665, 9666, 9667, 9668, 9669, 9670, 9671, 9672, 9673, 9674, 9675, 9676, 9677, 9678, 9679, 9680, 9681, 9682, 9683, 9684, 9685, 9686, 9687, 9688, 9689, 9690, 9691, 9692, 9693, 9694, 9695, 9696, 9697, 9698, 9699, 9700, 9701, 9702, 9703, 9704, 9705, 9706, 9707, 9708, 9709, 9710, 9711, 9712, 9713, 9714, 9715, 9716, 9717, 9718, 9719, 9720, 9721, 9722, 9723, 9724, 9725, 9726, 9727, 9728, 9729, 9730, 9731, 9732, 9733, 9734, 9735, 9736, 9737, 9738, 9739, 9740, 9741, 9742, 9743, 9744, 9745, 9746, 9747, 9748, 9749, 9750, 9751, 9752, 9753, 9754, 9755, 9756, 9757, 9758, 9759, 9760, 9761, 9762, 9763}
+
     else
-        return S(ids:key_filter(windower.wc_match-{name}))
+        return flatten(S(all_ids:key_filter(windower.wc_match-{name})))
 
     end
 end
@@ -184,7 +223,7 @@ windower.register_event('incoming chunk', function(id, data)
             return
         end
 
-        if id == 0x020 and settings.AutoDrop and code.drop:contains(chunk.Item) then
+        if id == 0x020 and settings.AutoDrop and code.drop:contains(chunk.Item) and chunk.Status == 0 then
             drop(chunk.Item, chunk.Index, chunk.Count)
         else
             -- Don't need to stack in the other case, as a new inventory packet will come in after the drop anyway
@@ -228,7 +267,7 @@ windower.register_event('addon command', function(command1, command2, ...)
                 error('No items found that match: %s':format(name))
                 return
             end
-            lotpassdrop(command1, command2, ids)            
+            lotpassdrop(command1, command2, ids)
 
             if global then
                 windower.send_ipc_message('treasury %s %s %s':format(command1, command2, ids:concat(' ')))
@@ -248,12 +287,12 @@ windower.register_event('addon command', function(command1, command2, ...)
         end
 
     elseif command1 == 'passall' then
-        for slot_index, item_table in pairs(windower.ffxi.get_items().treasure) do 
+        for slot_index, item_table in pairs(windower.ffxi.get_items().treasure) do
             windower.ffxi.pass_item(slot_index)
         end
-        
+
     elseif command1 == 'lotall' then
-        for slot_index, item_table in pairs(windower.ffxi.get_items().treasure) do 
+        for slot_index, item_table in pairs(windower.ffxi.get_items().treasure) do
             windower.ffxi.lot_item(slot_index)
         end
 
@@ -320,12 +359,11 @@ windower.register_event('addon command', function(command1, command2, ...)
         print('    \\cs(255,255,255)autostack [on|off]\\cr - Enables/disables (or toggles) the autostack feature')
         print('    \\cs(255,255,255)delay <value>\\cr - Allows you to change the delay of actions (default: 0)')
 
-
     end
 end)
 
 --[[
-Copyright © 2014-2015, Windower
+Copyright © 2014-2018, Windower
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
