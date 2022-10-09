f2b8a0f07502047dd6b6501e1781402bb5450bbd
aragan <64033062+aragan@users.noreply.github.com>
2022-10-09 10:59:27 +0300

    Add files via upload

--- aecho/aecho.lua
+++ aecho/aecho.lua
@@ -1,10 +1,10 @@
 --[[
 Copyright (c) 2013, Ricky Gall
 All rights reserved.
-
+ 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-
+ 
     * Redistributions of source code must retain the above copyright
     notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@ modification, are permitted provided that the following conditions are met:
     * Neither the name of <addon name> nor the
     names of its contributors may be used to endorse or promote products
     derived from this software without specific prior written permission.
-
+ 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,13 +25,13 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
-
-
+ 
+ 
 _addon.name = 'AEcho'
 _addon.version = '2.02'
 _addon.author = 'Nitrous (Shiva)'
 _addon.command = 'aecho'
-
+ 
 require('tables')
 require('strings')
 require('logger')
@@ -39,33 +39,94 @@ require('sets')
 config = require('config')
 chat = require('chat')
 res = require('resources')
-
+ 
 defaults = {}
-defaults.buffs = S{	"light arts","addendum: white","penury","celerity","accession","perpetuance","rapture",
-                    "dark arts","addendum: black","parsimony","alacrity","manifestation","ebullience","immanence",
-                    "stun","petrified","silence","stun","sleep","slow","paralyze"
+defaults.buffs = S{ "parsimony","dark arts","rapture","perpetuance","petrified","paralyze","light arts","slow",
+"accession","manifestation","addendum: white","addendum: black","celerity","ebullience","stun","sleep","alacrity",
+"immanence","penury","poison","blind","silence","disease","doom","charm","amnesia","curse","bind","gravity","addle",
+"kaustra","mute","plague","bane","burn","frost","rasp","shock","terror","drown","dia","bio","flash","str down",
+"dex down","vit down","agi down","int down","mnd down","chr down","hp max down","mp max down","tp max down","helix",
+"inhibit tp","accuracy down","attack down","evasion down","zombie","defense down","magic accuracy down","magic attack down",
+"magic evasion down","magic defense down","weakness"
                 }
 defaults.alttrack = true
 defaults.sitrack = true
-
+ 
 settings = config.load(defaults)
-
+ 
 autoecho = true
-
+ 
 windower.register_event('gain buff', function(id)
     local name = res.buffs[id].english
     for key,val in pairs(settings.buffs) do
         if key:lower() == name:lower() then
             if name:lower() == 'silence' and autoecho then
                 windower.send_command('input /item "Echo Drops" '..windower.ffxi.get_player()["name"])
-            end
-            if settings.alttrack then
-                windower.send_command('send @others atc '..windower.ffxi.get_player()["name"]..' - '..name)
+            elseif name:lower() == 'blind' and autoecho then
+                windower.send_command('input /item "Eye Drops" '..windower.ffxi.get_player()["name"])
+            elseif name:lower() == 'paralyze' and autoecho then
+                windower.send_command('input /item "Remedy" '..windower.ffxi.get_player()["name"])
+            elseif name:lower() == 'disease' and autoecho then
+                windower.send_command('input /item "Remedy" '..windower.ffxi.get_player()["name"])
+            elseif name:lower() == 'poison' and autoecho then
+                windower.send_command('input /item "Antidote" '..windower.ffxi.get_player()["name"])
+            elseif name:lower() == 'doom' and autoecho then
+                windower.send_command('input /item "Holy Water" '..windower.ffxi.get_player()["name"])
+            elseif name:lower() == 'curse' and autoecho then
+                windower.send_command('input /item "Holy Water" '..windower.ffxi.get_player()["name"])
+            elseif name:lower() == 'bane' and autoecho then
+                windower.send_command('input /item "Holy Water" '..windower.ffxi.get_player()["name"])
+            elseif name:lower() == 'stun' and autoecho then
+                windower.send_command('input /p I am stunned!!')
+            elseif name:lower() == 'slow' and autoecho then
+                windower.send_command('input /p I have been slowed!!')
+            elseif name:lower() == 'sleep' and autoecho then
+                windower.send_command('input /p Help I have been put to sleep!!')
+            elseif name:lower() == 'petrified' and autoecho then
+                windower.send_command('input /p Help I am petrified!!')
+            elseif name:lower() == 'bind' and autoecho then
+                windower.send_command('input /p Help I am bound!!')
+            elseif name:lower() == 'charm' and autoecho then
+                windower.send_command('input /p Help I am charmed!! Sleep me or run.')
+            elseif name:lower() == 'amnesia' and autoecho then
+                windower.send_command('input /p I have amnesia!!')
+            elseif name:lower() == 'terror' and autoecho then
+                windower.send_command('input /p I am terrorized!!')
+            elseif name:lower() == 'flash' and autoecho then
+                windower.send_command('input /p I have been afflicted with flash!!')
+            elseif (name:lower() == 'burn' or name:lower() == 'bio' or name:lower() == 'frost' or name:lower() == 'dia' or name:lower() == 'shock' or name:lower() == 'rasp' or name:lower() == 'drown' or name:lower() == 'choke') and autoecho then
+                windower.send_command('input /p I have been afflicted with a DoT!! Slowly losing HP.')
+            elseif name:lower() == 'helix' and autoecho then
+                windower.send_command('input /p I have been afflicted with a Helix!! Severe loss of HP.')
+            elseif (name:lower() == 'str down' or name:lower() == 'agi down' or name:lower() == 'dex down' or name:lower() == 'mnd down' or name:lower() == 'int down' or name:lower() == 'vit down' or name:lower() == 'chr down') and autoecho then
+                windower.send_command('input /p I have been afflicted one or more attributes being reduced!!')
+            elseif name:lower() == 'plague' and autoecho then
+                windower.send_command('input /p I have been afflicted with a plague!! Losing TP and MP.')
+            elseif name:lower() == 'zombie' and autoecho then
+                windower.send_command('input /p I have been afflicted with zombie!! Unable to be cured.')
+            elseif name:lower() == 'weakness' and autoecho then
+                windower.send_command('input /p I have been afflicted by weakness!! HP Halved.')
+            elseif name:lower() == 'gravity' and autoecho then
+                windower.send_command('input /p I have been afflicted by gravity!! Movement hindered.')
+            elseif name:lower() == 'evasion down' and autoecho then
+                windower.send_command('input /p My evasion has been reduced!!')
+            elseif name:lower() == 'attack down' and autoecho then
+                windower.send_command('input /p My attack has been reduced!!')
+            elseif name:lower() == 'defense down' and autoecho then
+                windower.send_command('input /p My defense has been reduced!!')
+            elseif name:lower() == 'accuracy down' and autoecho then
+                windower.send_command('input /p My accuracy has been reduced!!')
+            elseif name:lower() == 'hp max down' and autoecho then
+                windower.send_command('input /p My HP has been reduced!!')
+            elseif name:lower() == 'mp max down' and autoecho then
+                windower.send_command('input /p My MP has been reduced!!')
+            elseif name:lower() == 'tp max down' and autoecho then
+                windower.send_command('input /p My TP has been reduced!!')
             end
         end
     end
 end)
-
+ 
 windower.register_event('incoming text', function(old,new,color)
     if settings.sitrack then
         local sta,ea,txt = string.find(new,'The effect of ([%w]+) is about to wear off.')
@@ -75,7 +136,7 @@ windower.register_event('incoming text', function(old,new,color)
     end
     return new,color
 end)
-
+ 
 windower.register_event('addon command', function(...)
     local args = {...}
     if args[1] ~= nil then
