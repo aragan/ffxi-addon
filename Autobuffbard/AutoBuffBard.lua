22ba60323b808b6c84d7b7da5ddd5ee40ee9a0e4
aragan <64033062+aragan@users.noreply.github.com>
2024-06-24 15:13:14 +0300

    Add files via upload

--- Autobuffbard/AutoBuffBard.lua
+++ Autobuffbard/AutoBuffBard.lua
@@ -1,7 +1,11 @@
-_addon.name = 'AutoBardBuff'
+_addon.name = 'AutoBuffBard'
 _addon.author = 'Helmaru'
 _addon.version = '2.2.0'
 _addon.commands = {'abb','ABB',}
+
+--modification by Xenodeus
+--added dynamic pianissimo list
+--edited behavior for multiple placeholder songs all at once + clarion call placeholder song (switch via boolean original_logic)
 
 local abf_active = true
 local abf_buffer = 0
@@ -10,6 +14,8 @@ local abf_runes = {}
 local abf_autorune = false
 local abf_phsongs = true
 
+local original_logic = false
+local abf_pianissimo_players = {}
 
 local slow_recast = "7";
 local nitro_reacst = "5";
@@ -20,7 +26,30 @@ windower.register_event('addon command', function(cmd, ...)
 	local args = string.lower(table.concat({...},' '))
 	--windower.add_to_chat(8,dump({...}))
 	if string.lower(cmd) == "help" then
-		windower.add_to_chat(8,'[AutoBardBuff] USAGE: //abb [setname] [nitro|noph]')
+		windower.add_to_chat(8,'[AutoBuffBard] USAGE: //abb [setname] [nitro|noph]')
+		windower.add_to_chat(8,'[AutoBuffBard] USAGE: //abb set [pianissimoSetName] [playername]')
+		windower.add_to_chat(8,'[AutoBuffBard] USAGE: //abb reset')
+	elseif string.lower(cmd) == "set" then
+		local params = {...}
+		if select('#', ...) ~= 2 then
+			windower.add_to_chat(8, '[AutoBuffBard] ****** ['..cmd..' '..args..' CANCELED - Not enough parameters] ******')
+		else
+			if params[1] and params[2] then
+				local pianissimo_set = string.lower(params[1])
+				local playername = string.lower(params[2])
+				if not bff_pianissimo_sets[pianissimo_set] then
+					windower.add_to_chat(8, '[AutoBuffBard] ****** ['..params[1]..' CANCELED - Set Not Found] ******')
+				else
+					abf_pianissimo_players[playername] = pianissimo_set
+					windower.add_to_chat(8, '[AutoBuffBard] ****** ['..playername..' ADDED with the Set '..pianissimo_set..'] ******')
+				end
+			else
+				windower.add_to_chat(8, '[AutoBuffBard] ****** ['..cmd..' '..args..' CANCELED - Not enough parameters] ******')
+			end
+		end
+	elseif string.lower(cmd) == "reset" then
+		abf_pianissimo_players = {}
+		windower.add_to_chat(8, '[AutoBuffBard] ****** ['..cmd..' DONE] ******')
 	else
 		if args == 'noph' then
 			abf_phsongs = false
@@ -28,7 +57,7 @@ windower.register_event('addon command', function(cmd, ...)
 			abf_phsongs = true
 		end
 		if args == 'nonitro' then
-			windower.add_to_chat(8,'[AutoBardBuff] Suppressing nitro!')
+			windower.add_to_chat(8,'[AutoBuffBard] Suppressing nitro!')
 			do_buff(cmd, true)
 		else
 			do_buff(cmd, false)
@@ -42,15 +71,15 @@ function do_buff(cmd, nonitro)
 	local recast = slow_recast
 	if hasbuff(6) then
 		buffstring = 'input /item "Remedy" <me>;wait 5;'
-        windower.add_to_chat(8, '[AutoBardBuff] ****** [SILENCED - Using Remedy] ******')
+        windower.add_to_chat(8, '[AutoBuffBard] ****** [SILENCED - Using Remedy] ******')
 	end
 	if is_impaired() then
-        windower.add_to_chat(8, '[AutoBardBuff] ****** ['..cmd..' CANCELED - impaired] ******')
+        windower.add_to_chat(8, '[AutoBuffBard] ****** ['..cmd..' CANCELED - impaired] ******')
 		return
 	end
-	windower.add_to_chat(8,'[AutoBardBuff] '..cmd)
+	windower.add_to_chat(8,'[AutoBuffBard] '..cmd)
 	if moving then 
-		windower.add_to_chat(8, '[AutoBardBuff] ****** ['..cmd..' CANCELED - Moving] ******')
+		windower.add_to_chat(8, '[AutoBuffBard] ****** ['..cmd..' CANCELED - Moving] ******')
 		return 
 	end 
 	if windower.ffxi.get_ability_recasts()[109]== 0 and windower.ffxi.get_ability_recasts()[110]== 0 and not nonitro then
@@ -59,28 +88,58 @@ function do_buff(cmd, nonitro)
 	end
 	
 	if not bff_sets[cmd] then
-		windower.add_to_chat(8, '[AutoBardBuff] ****** ['..cmd..' CANCELED - Set Not Found] ******')
+		windower.add_to_chat(8, '[AutoBuffBard] ****** ['..cmd..' CANCELED - Set Not Found] ******')
 	end
 	
-	local int = 1
-	local ph_song = bff_ph_song
-	for _,entry in pairs(bff_sets[cmd]) do
-		if int == 2 and windower.ffxi.get_ability_recasts()[48]== 0 then
-			buffstring = buffstring..'input /ja "Marcato" <me>;wait 4;'
-		end
-		if int <= 4 or hasbuff(499) then
-			if int > 2 and abf_phsongs and entry ~= "Aria of Passion" then
-				buffstring = buffstring..'input /ma "'..ph_song..'" <me>;wait '..recast..';'
-				ph_song = bff_ph_song..' '..tostring(int)
+	if original_logic then
+		local int = 1
+		local ph_song = bff_ph_song
+		for _,entry in pairs(bff_sets[cmd]) do
+			if int == 2 and windower.ffxi.get_ability_recasts()[48]== 0 then
+				buffstring = buffstring..'input /ja "Marcato" <me>;wait 4;'
 			end
-			buffstring = buffstring..'input /ma "'..entry..'" <me>;wait '..recast..';'
+			if int <= 4 or hasbuff(499) then
+				if int > 2 and abf_phsongs and entry ~= "Aria of Passion" then
+					buffstring = buffstring..'input /ma "'..ph_song..'" <me>;wait '..recast..';'
+					ph_song = bff_ph_song..' '..tostring(int)
+				end
+				buffstring = buffstring..'input /ma "'..entry..'" <me>;wait '..recast..';'
+			end
+			int = int + 1
 		end
-		int = int + 1
-    end
+	else
+		local int = 1
+		local ph_songs = bff_ph_songs
+		local ph_songs_todo = true
+		for _,entry in pairs(bff_sets[cmd]) do
+			if int == 2 and windower.ffxi.get_ability_recasts()[48]== 0 then
+				buffstring = buffstring..'input /ja "Marcato" <me>;wait 4;'
+			end
+			if int <= 4 or hasbuff(499) then
+				if int > 2 and abf_phsongs and ph_songs_todo and entry ~= "Aria of Passion" then
+					for _,ph_song in pairs(bff_ph_songs) do
+						buffstring = buffstring..'input /ma "'..ph_song..'" <me>;wait '..recast..';'
+					end
+					if hasbuff(499) and bff_ph_cc_song then
+						buffstring = buffstring..'input /ma "'..bff_ph_cc_song..'" <me>;wait '..recast..';'
+					end
+					ph_songs_todo = false
+				end
+				buffstring = buffstring..'input /ma "'..entry..'" <me>;wait '..recast..';'
+			end
+			int = int + 1
+		end
+	end
 	
 	for key,entry in pairs(bff_pianissimo) do
 		buffstring = buffstring..'input /ja "Pianissimo" <me>;wait 3;input /ma "'..entry..'" '..key..';wait '..recast..';'
     end
+
+	for player_name,pianissimo_set in pairs(abf_pianissimo_players) do
+		for key,song_name in pairs(bff_pianissimo_sets[pianissimo_set]) do
+			buffstring = buffstring..'input /ja "Pianissimo" <me>;wait 3;input /ma "'..song_name..'" '..player_name..';wait '..recast..';'
+		end
+	end
 	
 	windower.add_to_chat(8, buffstring)
 	windower.send_command(buffstring)
@@ -128,7 +187,7 @@ function silent_check_amnesia()
 end
 function is_impaired()
 	if hasbuff(28) or hasbuff(7) or hasbuff(19) or hasbuff(193) or hasbuff(10) then
-		windower.add_to_chat(8, '[AutoBardBuff] ****** ['..cmd..' CANCELED - Amnesia/Impaired] ******')
+		windower.add_to_chat(8, '[AutoBuffBard] ****** ['..cmd..' CANCELED - Amnesia/Impaired] ******')
 		return true
 		
 	else
--- Autobuffbard/abb_sets.lua
+++ Autobuffbard/abb_sets.lua
@@ -1,11 +1,12 @@
 bff_sets = {};
 
--- ************************************************************************************ --
--- DEFINE YOUR SETS HERE, SCOND SONT GETS MARCATO (SO WE DONT OVWRITE IT WITH PIANISSIMO)
+-- ************************************************************************************* --
+-- DEFINE YOUR SETS HERE, SECOND SONG GETS MARCATO (SO WE DONT OVWRITE IT WITH PIANISSIMO)
+-- EVERY SET NAME MUST BE LOWERCASE
 bff_sets.haste2 = {'Valor Minuet IV', 'Honor March', 'Valor Minuet V',  'Blade Madrigal', "Knight's Minne V"}
 bff_sets.aria2 = {'Valor Minuet IV', 'Honor March',  "Aria of Passion", 'Valor Minuet V', 'Blade Madrigal'}
 bff_sets.sherzo = {'Valor Minuet IV', 'Honor March', 'Valor Minuet V', "Sentinel's Scherzo", 'Blade Madrigal'}
-bff_sets.haste2Acc = {'Valor Minuet IV', 'Honor March', 'Valor Minuet V', 'Blade Madrigal', "Sword Madrigal"}
+bff_sets.haste2acc = {'Valor Minuet IV', 'Honor March', 'Valor Minuet V', 'Blade Madrigal', "Sword Madrigal"}
 bff_sets.haste1 = {'Valor Minuet IV', 'Honor March', 'Valor Minuet V', 'Victory March', "Blade Madrigal"}
 bff_sets.aria1 = {'Valor Minuet IV', 'Honor March',  "Aria of Passion", 'Valor Minuet V', 'Victory March'}
 bff_sets.sheol = {'Valor Minuet IV', 'Honor March', 'Valor Minuet V', 'Victory March',  "Knight's Minne V"}
@@ -14,15 +15,23 @@ bff_sets.fire = {'Blade Madrigal', 'Honor March', 'Valor Minuet V', 'Fire Carol'
 bff_sets.mboze = {'Valor Minuet IV', 'Honor March', 'Valor Minuet V', "Adventurer's Dirge", "Sentinel's Scherzo"}
 bff_sets.magic = {'Blade Madrigal', 'Honor March', 'Logical Etude', "Sage Etude", "Sentinel's Scherzo"}
 bff_sets.skill = {'Blade Madrigal', 'Honor March', 'Sword Madrigal', "Victory March", "Sentinel's Scherzo"}
+bff_sets.seg = {'Herculean Etude', 'Honor March', 'Valor Minuet V', "Valor Minuet III", "Valor Minuet IV"}
+bff_sets.ph = {"Army's Paeon","Valor Minuet","Knight's Minne","Sheepfoe Mambo","Shining Fantasia"}
 
 --PIANISSIMO TARGET (ONLY ONE ATM, i.e. BALLAD FOR HEALER)
 --bff_pianissimo = {['Anitamaru']= "Mage's Ballad III", ['Limaru']= "Mage's Ballad III"}
 --bff_pianissimo = {['Anitamaru']= "Mage's Ballad III"}
-bff_pianissimo = {['Limaru']= "Mage's Ballad III"}
+--bff_pianissimo = {['Limaru']= "Mage's Ballad III"}
 --bff_pianissimo = {['Spatz']= "Mage's Ballad III"}
+bff_pianissimo = {}
+
+bff_pianissimo_sets = {}
+bff_pianissimo_sets.refresh = {"Mage's Ballad III"}
 
 --PLACEHOLDER FOR BUFFING
-bff_ph_song = "Army's Paeon"
+--bff_ph_song = "Army's Paeon"
+bff_ph_songs = {'Shining Fantasia', 'Herb Pastoral'}
+bff_ph_cc_song = "Army's Paeon" --clarian call song
 
 -- END OF CUSTOM STUFF
 -- ************************************************************************************ --
